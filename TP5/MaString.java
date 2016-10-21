3afcbf85a6669aab554199cc2df77684a9fd3e19
Ccl <cecilia.mcszk@gmail.com>
2016-10-21 09:18:01 +0200

    Add TP% (empty files)

---
+++ TP5/MaString.java
@@ -0,0 +1,17 @@
+package mesStrings;
+
+public class MaString{
+	private char[] lesCaracteres;
+	public MaString() {
+		System.out.println(lesCaracteres);
+	}
+	//…
+	public String toString(){
+		return new String(lesCaracteres);
+	}
+	
+	public static void main(String[] args) {
+		//
+	}
+
+}
